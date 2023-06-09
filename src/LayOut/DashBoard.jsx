964f672d9513081c8a75c0122aa255d30aa7381c
hasanali112 <mdhasan.ru.ro@gmail.com>
2023-06-09 22:16:31 +0600

    registration user store into database

--- src/LayOut/DashBoard.jsx
+++ src/LayOut/DashBoard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from '../assets/image/logo.png'
 import { Link, Outlet } from "react-router-dom";
-import { FaHome } from "react-icons/fa";
+import { FaHome, FaRegPlusSquare, FaRegUser, FaUserCheck, FaUserCog, FaUserFriends, FaUsersCog, } from "react-icons/fa";
 
 
 const DashBoard = () => {
@@ -31,12 +31,12 @@ const DashBoard = () => {
           </div>
           <div className="divider bg-white h-1"></div>
           <li><Link><FaHome></FaHome>Home</Link></li>
-          <li><Link to='/dashboard/myselectedclasses'>My Selected Classes</Link></li>
-          <li><Link to='/dashboard/myenrolledclasses'>My Enrolled Classes</Link></li>
-          <li><Link to='/dashboard/addaclass'>Add a Class</Link></li>
-          <li><Link to='/dashboard/myclass'>My Classes</Link></li>
-          <li><Link to='/dashboard/manageclasses'>Manage Classes</Link></li>
-          <li><Link to='/dashboard/manageusers'>Manage Users</Link></li>
+          <li><Link to='/dashboard/myselectedclasses'><FaUserFriends></FaUserFriends> My Selected Classes</Link></li>
+          <li><Link to='/dashboard/myenrolledclasses'><FaUserCheck></FaUserCheck>  My Enrolled Classes</Link></li>
+          <li><Link to='/dashboard/addaclass'><FaRegPlusSquare></FaRegPlusSquare> Add a Class</Link></li>
+          <li><Link to='/dashboard/myclass'><FaRegUser></FaRegUser> My Classes</Link></li>
+          <li><Link to='/dashboard/manageclasses'><FaUserCog></FaUserCog>  Manage Classes</Link></li>
+          <li><Link to='/dashboard/manageusers'><FaUsersCog></FaUsersCog> Manage Users</Link></li>
           <div className="divider bg-white h-1"></div>
         </ul>
       </div>
--- src/Pages/Registration.jsx
+++ src/Pages/Registration.jsx
@@ -1,59 +1,88 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState } from "react";
 import Lottie from "lottie-react";
 import groovyWalkAnimation from "../../public/login.json";
 import { useForm } from "react-hook-form";
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as Yup from 'yup';
-import { AuthContext } from '../Providers/AuthProvider';
-import { Link } from 'react-router-dom';
-import { FaEye } from 'react-icons/fa';
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
+import { AuthContext } from "../Providers/AuthProvider";
+import { Link } from "react-router-dom";
+import { FaEye } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const Registration = () => {
-  const{createUser, userUpdate} = useContext(AuthContext)
-  const [show, setShow] = useState(false)
-  const[showPass, setShowPass] = useState(false)
+  const { createUser, userUpdate } = useContext(AuthContext);
+  const [show, setShow] = useState(false);
+  const [showPass, setShowPass] = useState(false);
 
   const validationSchema = Yup.object().shape({
     password: Yup.string()
-        .required('Password is required')
-        .min(6, 'Password must be at least 6 characters')
-        .matches(/(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/,'Password must contain at least one lowercase letter, one uppercase letter,  one digit and one special character'
-        ),
+      .required("Password is required")
+      .min(6, "Password must be at least 6 characters")
+      .matches(
+        /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/,
+        "Password must contain at least one lowercase letter, one uppercase letter,  one digit and one special character"
+      ),
     confirmPassword: Yup.string()
-        .required('Confirm Password is required')
-        .oneOf([Yup.ref('password')], 'Passwords must match')
-        
-});
-   const formOptions = { resolver: yupResolver(validationSchema) };
-    const { register, handleSubmit, reset,  formState: { errors } } = useForm(formOptions);
-    
-    const onSubmit = data => {
-      createUser(data.email, data.password)
-      .then(result=>{
+      .required("Confirm Password is required")
+      .oneOf([Yup.ref("password")], "Passwords must match"),
+  });
+  const formOptions = { resolver: yupResolver(validationSchema) };
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm(formOptions);
+
+  const onSubmit = (data) => {
+    createUser(data.email, data.password)
+      .then((result) => {
         const createdUser = result.user;
-        console.log(createdUser)
+        console.log(createdUser);
         userUpdate(data.name, data.photo)
-        .then(()=>{
-          reset()
-        })
-        .catch(error=>{
-          console.log(error)
-        })
+          .then(() => {
+            const userSave = { name: data.name, email: data.email };
+            fetch("http://localhost:5000/users", {
+              method: "POST",
+              headers: {
+                "content-type": "application/json",
+              },
+              body: JSON.stringify(userSave),
+            })
+              .then((res) => res.json())
+              .then((data) => {
+                if (data.insertedId) {
+                  reset();
+                  Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: "Account Created Successfully",
+                    showConfirmButton: false,
+                    timer: 1500,
+                  });
+                }
+              });
+            reset();
+          })
+          .catch((error) => {
+            console.log(error);
+          });
       })
-      .catch(error=>{
-        console.log(error)
-      })
-      
-    };
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-
-
-    return (
-        <div className="hero min-h-screen bg-base-200">
+  return (
+    <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row-reverse mt-40">
         <div className="text-center">
           <h1 className="text-5xl font-bold ">Registration now!</h1>
-          <Lottie animationData={groovyWalkAnimation} loop={true} className="w-[600px] h-[600px]"/>
+          <Lottie
+            animationData={groovyWalkAnimation}
+            loop={true}
+            className="w-[600px] h-[600px]"
+          />
         </div>
         <div className="card flex-shrink-0 w-full mr-10 max-w-md shadow-2xl bg-base-100">
           <form onSubmit={handleSubmit(onSubmit)} className="card-body">
@@ -64,10 +93,13 @@ const Registration = () => {
               <input
                 type="text"
                 {...register("name", { required: true })}
+                name="name"
                 placeholder="Enter your name"
                 className="input input-bordered"
               />
-              {errors.name && <span className="text-red-500">Name is required</span>}
+              {errors.name?.type === "required" && (
+                <p className="text-red-600 pt-4">Name is required</p>
+              )}
             </div>
             <div className="form-control">
               <label className="label">
@@ -79,7 +111,9 @@ const Registration = () => {
                 placeholder="Enter your email"
                 className="input input-bordered"
               />
-              {errors.email && <span className="text-red-500">Email is required</span>}
+              {errors.email?.type === "required" && (
+                <p className="text-red-600 pt-4">Email is required</p>
+              )}
             </div>
             <div className="form-control">
               <label className="label">
@@ -91,43 +125,77 @@ const Registration = () => {
                 placeholder="Enter your photo url"
                 className="input input-bordered"
               />
-              {errors.password && <span className="text-red-500">Photo url is required</span>}
+              {errors.password && (
+                <span className="text-red-500">Photo url is required</span>
+              )}
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Password</span>
               </label>
               <input
-                 type={show ? 'text' : 'password'}
-                {...register("password", { required: true })}
-                placeholder="Enter your password"
+                type={show ? "text" : "password"}
+                {...register("password", {
+                  required: true,
+                  minLength: 6,
+                  pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/,
+                })}
+                placeholder="password"
                 className="input input-bordered"
               />
-               <FaEye onClick={()=> setShow(!show)} className="-mt-8  ml-[350px]"></FaEye>
-              {errors.password && <span className="text-red-500">Password must contain at least one lowercase letter, one uppercase letter,  one digit and one special character</span>}
+              <FaEye
+                onClick={() => setShow(!show)}
+                className="-mt-8  ml-[350px]"
+              ></FaEye>
+              {errors.password?.type === "required" && (
+                <p className="text-red-600 pt-4">Password is required</p>
+              )}
+              {errors.password?.type === "minLength" && (
+                <p className="text-red-600 pt-4">Password must be 6 characters</p>
+              )}
+              {errors.password?.type === "pattern" && (
+                <p className="text-red-600 pt-4">
+                  Password must have one Uppercase one lower case, one number
+                  and one special character.
+                </p>
+              )}
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Confirm password</span>
               </label>
               <input
-                type={showPass ? 'text' : 'password'}
+                type={showPass ? "text" : "password"}
                 {...register("confirmPassword", { required: true })}
                 placeholder="Enter your password"
                 className="input input-bordered"
               />
-               <FaEye onClick={()=> setShowPass(!showPass)} className="-mt-8 ml-[350px]"></FaEye>
-               {errors.confirmPassword && <span className="text-red-500">Passwords must match</span>}
+              <FaEye
+                onClick={() => setShowPass(!showPass)}
+                className="-mt-8 ml-[350px]"
+              ></FaEye>
+              {errors.confirmPassword && (
+                <span className="text-red-500 pt-4">Passwords must match</span>
+              )}
             </div>
             <div className="form-control mt-6">
-              <input className="btn btn-primary" type="submit" value="Sign Up" />
+              <input
+                className="btn btn-primary"
+                type="submit"
+                value="Sign Up"
+              />
             </div>
-            <h3>Already have an account? Please <Link className='text-green-500' to='/login'>LogIn</Link></h3>
+            <h3>
+              Already have an account? Please
+              <Link className="text-green-500" to="/login">
+                LogIn
+              </Link>
+            </h3>
           </form>
         </div>
       </div>
     </div>
-    );
+  );
 };
 
 export default Registration;
